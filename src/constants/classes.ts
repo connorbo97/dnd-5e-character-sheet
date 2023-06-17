e1736b403ad3533fb1d4b0757d9f2fbabf545818
Connor Borden <connor.b@taboola.com>
2023-06-17 14:22:54 -0700

    add labels

--- src/constants/classes.ts
+++ src/constants/classes.ts
@@ -16,19 +16,19 @@ export enum CLASSES {
   WIZARD = 'WIZARD',
 }
 
-export type ClassConfig = { hitDice: DICE };
+export type ClassConfig = { hitDice: DICE; label: string };
 export const CLASS_CONFIGS: { [c in CLASSES]: ClassConfig } = {
-  [CLASSES.ARTIFICER]: { hitDice: DICE.d8 },
-  [CLASSES.BARBARIAN]: { hitDice: DICE.d12 },
-  [CLASSES.BARD]: { hitDice: DICE.d8 },
-  [CLASSES.CLERIC]: { hitDice: DICE.d8 },
-  [CLASSES.DRUID]: { hitDice: DICE.d8 },
-  [CLASSES.FIGHTER]: { hitDice: DICE.d10 },
-  [CLASSES.MONK]: { hitDice: DICE.d8 },
-  [CLASSES.PALADIN]: { hitDice: DICE.d10 },
-  [CLASSES.RANGER]: { hitDice: DICE.d10 },
-  [CLASSES.ROGUE]: { hitDice: DICE.d8 },
-  [CLASSES.SORCERER]: { hitDice: DICE.d6 },
-  [CLASSES.WARLOCK]: { hitDice: DICE.d8 },
-  [CLASSES.WIZARD]: { hitDice: DICE.d6 },
+  [CLASSES.ARTIFICER]: { hitDice: DICE.d8, label: 'Artificer' },
+  [CLASSES.BARBARIAN]: { hitDice: DICE.d12, label: 'Barbarian' },
+  [CLASSES.BARD]: { hitDice: DICE.d8, label: 'Bard' },
+  [CLASSES.CLERIC]: { hitDice: DICE.d8, label: 'Cleric' },
+  [CLASSES.DRUID]: { hitDice: DICE.d8, label: 'Druid' },
+  [CLASSES.FIGHTER]: { hitDice: DICE.d10, label: 'Fighter' },
+  [CLASSES.MONK]: { hitDice: DICE.d8, label: 'Monk' },
+  [CLASSES.PALADIN]: { hitDice: DICE.d10, label: 'Paladin' },
+  [CLASSES.RANGER]: { hitDice: DICE.d10, label: 'Ranger' },
+  [CLASSES.ROGUE]: { hitDice: DICE.d8, label: 'Rogue' },
+  [CLASSES.SORCERER]: { hitDice: DICE.d6, label: 'Sorcerer' },
+  [CLASSES.WARLOCK]: { hitDice: DICE.d8, label: 'Warlock' },
+  [CLASSES.WIZARD]: { hitDice: DICE.d6, label: 'Wizard' },
 };
