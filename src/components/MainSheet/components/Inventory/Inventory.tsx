d9b13f3c07e8ec132e8adee9b1da238d1a58a459
Connor Borden <connor.b@taboola.com>
2023-06-17 10:12:16 -0700

    impleemnt inventory

--- src/components/MainSheet/components/Inventory/Inventory.tsx
+++ src/components/MainSheet/components/Inventory/Inventory.tsx
@@ -1,5 +1,42 @@
+import { useCharacterSheet } from 'providers/CharacterSheetProvider';
 import styles from './inventory.module.scss';
+import { Tag } from 'common/components/Tag/Tag';
 
 export const Inventory = () => {
-  return <div className={styles['container']}>Inventory</div>;
+  const { inventory } = useCharacterSheet();
+
+  return (
+    <div className={styles['container']}>
+      <h3>Inventory</h3>
+      <div>
+        {inventory.map(
+          (
+            {
+              label,
+              description,
+              equipped,
+              mods,
+              source,
+              total,
+              useAsResource,
+              weight,
+            },
+            index,
+          ) => (
+            <div key={index}>
+              <h5>{label}</h5>
+              <Tag label="description" value={description} />
+              <Tag label="source" value={source} />
+              <Tag label="equipped" value={equipped ? 'yes' : 'no'} />
+              <Tag label="resource?" value={useAsResource ? 'yes' : 'no'} />
+              <Tag label="total" value={total} />
+              <Tag label="weight" value={weight} />
+              <Tag label="total weight" value={total * weight} />
+              <Tag label="ac mod" value={mods?.ac?.join(' + ')} />
+            </div>
+          ),
+        )}
+      </div>
+    </div>
+  );
 };
