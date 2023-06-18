6015fbd71d0fbfff52d2054c7dc585834e88880d
Connor Borden <connor.b@taboola.com>
2023-06-17 17:32:15 -0700

    implement background more

--- src/components/MainSheet/components/Metadata/Metadata.tsx
+++ src/components/MainSheet/components/Metadata/Metadata.tsx
@@ -4,9 +4,11 @@ import { entries, identity } from 'lodash';
 import { CLASS_CONFIGS } from 'constants/classes';
 import { Tag } from 'common/components/Tag/Tag';
 import { RACE_CONFIGS } from 'constants/race';
+import { BACKGROUND_CONFIGS } from 'constants/backgrounds';
 
 export const Metadata = () => {
-  const { levels, race, alignment, profBonus } = useCharacterSheet();
+  const { levels, race, alignment, profBonus, background } =
+    useCharacterSheet();
   const raceLabel = RACE_CONFIGS[race.value].label;
 
   const fullRaceLabel = [race.subRace, raceLabel].filter(identity).join(' ');
@@ -31,6 +33,10 @@ export const Metadata = () => {
       <Tag label={'PB'} value={profBonus} />
       <Tag label={'Race'} value={fullRaceLabel} />
       <Tag label={'Alignment'} value={alignment} />
+      <Tag
+        label={'Background'}
+        value={BACKGROUND_CONFIGS[background.value].label}
+      />
     </div>
   );
 };
--- src/constants/backgrounds.ts
+++ src/constants/backgrounds.ts
@@ -3,3 +3,10 @@ export enum BACKGROUNDS {
   SOLDIER = 'SOLDIER',
   CUSTOM = 'CUSTOM',
 }
+
+export type BackgroundConfig = { label: string };
+export const BACKGROUND_CONFIGS: { [c in BACKGROUNDS]: BackgroundConfig } = {
+  [BACKGROUNDS.ACOLYTE]: { label: 'Acolyte' },
+  [BACKGROUNDS.SOLDIER]: { label: 'Soldier' },
+  [BACKGROUNDS.CUSTOM]: { label: 'Custom' },
+};
