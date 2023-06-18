f905921bffc39b9c1415bc815764b971b92c136e
Connor Borden <connor.b@taboola.com>
2023-06-17 18:24:08 -0700

    add passive perception field

--- src/components/MainSheet/components/Skills/Skills.tsx
+++ src/components/MainSheet/components/Skills/Skills.tsx
@@ -1,10 +1,11 @@
 import { SKILL_CONFIGS } from 'constants/skills';
 import styles from './skills.module.scss';
 import { useCharacterSheet } from 'providers/CharacterSheetProvider';
-import { STATS_CONFIGS } from 'constants/stats';
+import { STATS, STATS_CONFIGS } from 'constants/stats';
 import { getProficiencyBonus } from 'constants/proficiencyUtils';
 import { addNumberSign } from 'utils/stringUtils';
 import { ProficiencyButton } from 'common/components/ProficiencyButton/ProficiencyButton';
+import { Tag } from 'common/components/Tag/Tag';
 
 export const Skills = () => {
   const {
@@ -14,6 +15,9 @@ export const Skills = () => {
     profBonus,
   } = useCharacterSheet();
 
+  const calculateSkillModifier = (skillType, stat) =>
+    getStatModifier(stat) +
+    getProficiencyBonus(playerSkills[skillType], profBonus);
   return (
     <div className={styles['container']}>
       <h3>Skills</h3>
@@ -28,14 +32,15 @@ export const Skills = () => {
               onToggle={() => onToggleSkillProficiency(skillType)}
             />
             <span>
-              :{' '}
-              {addNumberSign(
-                getStatModifier(stat) +
-                  getProficiencyBonus(playerSkills[skillType], profBonus),
-              )}
+              : {addNumberSign(calculateSkillModifier(skillType, stat))}
             </span>
           </div>
         ))}
+        <br />
+        <Tag
+          label="Passive Perception"
+          value={10 + calculateSkillModifier('PERCEPTION', STATS.WIS)}
+        />
       </div>
     </div>
   );
