531b5b45fef4b2d1bc724242b86bec84aefbdbba
Connor Borden <connor.b@taboola.com>
2023-06-20 00:29:14 -0700

    remove test button

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import { useEffect } from 'react';
 import styles from './App.module.scss';
 import { Header } from './components/Header';
 import { Content } from 'components/Content';
-import { DICE } from 'constants/dice';
-import { STATS } from 'constants/stats';
-import { useChat } from 'providers/ChatProvider';
 import { loadDiceBox } from 'utils/diceBoxPackageUtils';
 
 function App() {
-  const { onRoll } = useChat();
-
   useEffect(() => {
     let diceBoxOptions;
     try {
@@ -29,14 +24,6 @@ function App() {
         <div id="dice-box-instructions">Click anywhere to dismiss</div>
       </div>
       <Header />
-      <button
-        onClick={() =>
-          onRoll([[3, DICE.d20], 1, STATS.STR, [6, DICE.d4]], {
-            description: 'test',
-          }).then((res) => console.log(res))
-        }>
-        Test
-      </button>
       <Content />
     </div>
   );
