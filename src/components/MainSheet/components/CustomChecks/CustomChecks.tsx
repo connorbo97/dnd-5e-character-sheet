094768e3fb34cfe11bd9f3352e0a535ac487c844
Connor Borden <connor.b@taboola.com>
2023-06-22 01:16:49 -0700

    implement custom checks with roll

--- src/components/MainSheet/components/CustomChecks/CustomChecks.tsx
+++ src/components/MainSheet/components/CustomChecks/CustomChecks.tsx
@@ -1,31 +1,74 @@
 import { useFullSheet } from 'providers/CharacterSheetProvider/useFullSheet';
 import styles from './customChecks.module.scss';
-import { STATS_CONFIGS } from 'constants/stats';
 import { addNumberSign } from 'utils/stringUtils';
 import { getProficiencyBonus } from 'constants/proficiencyUtils';
+import { ProficiencyButton } from 'common/components/ProficiencyButton/ProficiencyButton';
+import { Dropdown } from 'common/components/Dropdown/Dropdown';
+import { ATTACK_STAT_DROPDOWN_OPTIONS } from 'constants/attacks';
+import { useChat } from 'providers/ChatProvider';
+import { D20_DICE } from 'utils/rollableUtils';
+import { ChatType } from 'constants/chat';
 
 export const CustomChecks = () => {
-  const { getStatModifier, customChecks, onToggleToolProficiency, profBonus } =
-    useFullSheet();
+  const {
+    getStatModifier,
+    customChecks,
+    onChangeCustomCheckModByIndex,
+    onToggleCustomCheckProficiencyByIndex,
+    onChangeCustomCheckStatByIndex,
+    profBonus,
+  } = useFullSheet();
+  const { onRoll } = useChat();
 
   return (
     <div className={styles['container']}>
       <h3>Custom Checks</h3>
       <div>
-        {customChecks.map(({ label, stat }, index) => (
-          <div key={index} onClick={() => onToggleToolProficiency(index)}>
-            <u>
-              {label} ({stat ? STATS_CONFIGS[stat].shortLabel : 'NO STAT'})
-            </u>
-            <span>
-              :{' '}
-              {addNumberSign(
-                getStatModifier(stat) +
-                  getProficiencyBonus(customChecks[index], profBonus),
-              )}
-            </span>
-          </div>
-        ))}
+        {customChecks.map((check, index) => {
+          const modifier =
+            getStatModifier(check.stat) +
+            getProficiencyBonus(check, profBonus) +
+            (check.mod || 0);
+          const modifierWithSign = addNumberSign(modifier);
+          return (
+            <div key={index} className={styles['entry']}>
+              <span
+                className={styles['section']}
+                onClick={() =>
+                  onRoll([D20_DICE, modifier], {
+                    type: ChatType.BASIC,
+                    label: check.label,
+                    labelSuffix: `(${modifierWithSign})`,
+                  })
+                }>{`${check.label} (${modifierWithSign})`}</span>
+              <span className={styles['mod']}>
+                <ProficiencyButton
+                  config={check}
+                  onToggle={() => onToggleCustomCheckProficiencyByIndex(index)}
+                />
+                <span>+</span>
+                <Dropdown
+                  options={ATTACK_STAT_DROPDOWN_OPTIONS}
+                  value={check.stat || null}
+                  onChange={(e) =>
+                    onChangeCustomCheckStatByIndex(index, e.target.value)
+                  }
+                />
+                <span>+</span>
+                <input
+                  type="number"
+                  min={-99}
+                  max={99}
+                  placeholder="Mod"
+                  value={check.mod || ''}
+                  onChange={(e) =>
+                    onChangeCustomCheckModByIndex(index, e.target.value)
+                  }
+                />
+              </span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/providers/CharacterSheetProvider/useCustomChecks.tsx
+++ src/providers/CharacterSheetProvider/useCustomChecks.tsx
@@ -1,20 +1,40 @@
 import { useCharacterSheet } from 'providers/CharacterSheetProvider';
 import { useCallback } from 'react';
-import { iUpdate } from 'utils/lodashUtils';
+import { iSet, iUpdate } from 'utils/lodashUtils';
 
 export const useCustomChecks = () => {
   const { setSheet, sheet } = useCharacterSheet();
-  const { deathSaves } = sheet;
+  const { customChecks } = sheet;
 
-  const onToggleToolProficiency = useCallback(
-    (tool) => {
-      setSheet(iUpdate(sheet, `tools.${tool}.proficient`, (prev) => !prev));
+  const onToggleCustomCheckProficiencyByIndex = useCallback(
+    (index) => {
+      setSheet(
+        iUpdate(sheet, `customChecks.${index}.proficient`, (prev) => !prev),
+      );
+    },
+    [setSheet, sheet],
+  );
+  const onChangeCustomCheckStatByIndex = useCallback(
+    (index, value) => {
+      setSheet(iSet(sheet, `customChecks.${index}.stat`, value));
+    },
+    [setSheet, sheet],
+  );
+  const onChangeCustomCheckModByIndex = useCallback(
+    (index, value) => {
+      setSheet(
+        iUpdate(sheet, `customChecks.${index}.mod`, (prev) => {
+          return !isNaN(parseInt(value)) ? parseInt(value) : prev;
+        }),
+      );
     },
     [setSheet, sheet],
   );
 
   return {
-    deathSaves,
-    onToggleToolProficiency,
+    customChecks,
+    onToggleCustomCheckProficiencyByIndex,
+    onChangeCustomCheckStatByIndex,
+    onChangeCustomCheckModByIndex,
   };
 };
