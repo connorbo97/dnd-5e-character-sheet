5145a9ee884c9c132d49ae96300291a0bfd36324
Connor Borden <connor.b@taboola.com>
2023-06-22 21:39:52 -0700

    implement money inputs

--- src/components/MainSheet/components/Money/Money.tsx
+++ src/components/MainSheet/components/Money/Money.tsx
@@ -1,4 +1,3 @@
-import { useFullSheet } from 'providers/CharacterSheetProvider/useFullSheet';
 import styles from './money.module.scss';
 import { MONEY_CONFIGS } from 'constants/money';
 import { useMoney } from 'providers/CharacterSheetProvider/useMoney';
@@ -13,7 +12,15 @@ export const Money = () => {
         {Object.entries(MONEY_CONFIGS).map(([moneyType, { shortLabel }]) => (
           <div key={moneyType} className={styles['money-container']}>
             <span className={styles['label']}>{shortLabel}</span>
-            <span>{money[moneyType] || 0}</span>
+            <span></span>
+            <input
+              className={styles['input']}
+              type="number"
+              min={0}
+              max={9999999}
+              value={money[moneyType] || 0}
+              onChange={(e) => onChangeMoneyByType(moneyType, e.target.value)}
+            />
           </div>
         ))}
       </div>
