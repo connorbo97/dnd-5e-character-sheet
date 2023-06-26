a1245d8f11f3189792b1d6cc97fbc897c54894a9
Connor Borden <connor.b@taboola.com>
2023-06-25 17:09:03 -0700

    add enums

--- src/constants/race.ts
+++ src/constants/race.ts
@@ -46,14 +46,38 @@ export type RaceStatConfig = {
 //   };
 //   features: Array<{ label: string; description: string }>;
 // };
+export enum RACE_CONFIG_TYPE {
+  STATIC = 'STATIC',
+  CHOICE = 'CHOICE',
+}
+export enum RACE_CONFIG_FORMAT {
+  BASIC = 'BASIC',
+  STATS = 'STATS',
+  SPEED = 'SPEED',
+  PROFICIENCY = 'PROFICIENCY',
+  DROPDOWN = 'DROPDOWN',
+  FEATURE = 'FEATURE',
+}
+
 type RaceConfigs = {
-  [r in RACES]: { label: string; createConfig: any };
+  [r in RACES]: {
+    label: string;
+    createConfig: {
+      base: Array<{
+        type: RACE_CONFIG_TYPE;
+        format: RACE_CONFIG_FORMAT;
+        [s: string]: any;
+      }>;
+      subClassOptions?: Array<{ value: any; label: string }>;
+      subClass?: Array<any>;
+    };
+  };
 };
 
 export const RACE_CONFIGS: RaceConfigs = {
   [RACES.DRAGONBORN]: {
     label: 'Dragonborn',
-    createConfig: [],
+    createConfig: { base: [] },
     // createConfig: {
     //   stats: [
     //     { stat: STATS.CHA, value: 1 },
@@ -68,176 +92,195 @@ export const RACE_CONFIGS: RaceConfigs = {
   },
   [RACES.DWARF]: {
     label: 'Dwarf',
-    createConfig: [
-      {
-        type: 'STATIC',
-        format: 'STATS',
-        path: 'stats',
-        value: {
-          [STATS.CON]: 2,
-        },
-      },
-      {
-        type: 'STATIC',
-        format: 'BASIC',
-        path: 'creatureType',
-        value: CREATURE_TYPE.HUMANOID,
-        config: {
-          header: 'Creature Type',
-        },
-      },
-      {
-        type: 'STATIC',
-        format: 'BASIC',
-        path: 'size',
-        value: CREATURE_SIZE.MEDIUM,
-        config: {
-          header: 'Size',
-        },
-      },
-      {
-        type: 'STATIC',
-        format: 'SPEED',
-        path: 'speed',
-        value: [{ value: 25, type: 'Walking' }],
-      },
-      {
-        type: 'STATIC',
-        format: 'PROFICIENCY',
-        path: 'otherProficiencies',
-        value: [
-          { label: 'Battleaxe', category: 'Weapon' },
-          { label: 'Handaxe', category: 'Weapon' },
-          { label: 'Light Hammer', category: 'Weapon' },
-          { label: 'Warhammer', category: 'Weapon' },
-        ],
-        config: {
-          header: 'Weapon Proficiencies',
-        },
-      },
-      {
-        type: 'CHOICE',
-        format: 'DROPDOWN',
-        path: 'MULTI',
-        options: [
-          { value: "Smith's Tools", label: "Smith's Tools" },
-          { value: "Brewer's Tools", label: "Brewer's Tools" },
-          { value: "Mason's Tools", label: "Mason's Tools" },
-        ],
-        config: {
-          header: 'Tool Proficiency',
-          getFinalValue: (val) => {
-            return {
-              otherProficiencies: [{ label: val, category: 'Tool' }],
-              customChecks: [{ label: val, stat: STATS.STR }],
-            };
+    createConfig: {
+      base: [
+        {
+          type: RACE_CONFIG_TYPE.STATIC,
+          format: RACE_CONFIG_FORMAT.STATS,
+          path: 'stats',
+          value: {
+            [STATS.CON]: 2,
           },
         },
-      },
-      {
-        type: 'STATIC',
-        format: 'PROFICIENCY',
-        path: 'otherProficiencies',
-        value: [
-          { label: 'Common', category: 'Language' },
-          { label: 'Dwarvish', category: 'Language' },
-        ],
-        config: {
-          header: 'Languages',
-          subHeader: 'Language Proficiencies:',
-          description:
-            'You can speak, read, and write Common and Dwarvish. Dwarvish is full of hard consonants and guttural sounds, and those characteristics spill over into whatever other language a dwarf might speak.',
+        {
+          type: RACE_CONFIG_TYPE.STATIC,
+          format: RACE_CONFIG_FORMAT.BASIC,
+          path: 'creatureType',
+          value: CREATURE_TYPE.HUMANOID,
+          config: {
+            header: 'Creature Type',
+          },
         },
-      },
-      {
-        type: 'STATIC',
-        format: 'FEATURE',
-        path: 'features',
-        value: {
-          label: 'Dwarven Speed',
-          description: 'Your speed is not reduced by wearing heavy armor.',
+        {
+          type: RACE_CONFIG_TYPE.STATIC,
+          format: RACE_CONFIG_FORMAT.BASIC,
+          path: 'size',
+          value: CREATURE_SIZE.MEDIUM,
+          config: {
+            header: 'Size',
+          },
         },
-      },
-      {
-        type: 'STATIC',
-        format: 'FEATURE',
-        path: 'features',
-        value: {
-          label: 'Stonecutting',
-          description:
-            'Whenever you make an Intelligence (History) check related to the origin of stonework, you are considered proficient in the History skill and add double your proficiency bonus to the check, instead of your normal proficiency bonus.',
+        {
+          type: RACE_CONFIG_TYPE.STATIC,
+          format: RACE_CONFIG_FORMAT.SPEED,
+          path: 'speed',
+          value: [{ value: 25, type: 'Walking' }],
         },
-        config: {
-          header: 'Speed',
+        {
+          type: RACE_CONFIG_TYPE.STATIC,
+          format: RACE_CONFIG_FORMAT.PROFICIENCY,
+          path: 'otherProficiencies',
+          value: [
+            { label: 'Battleaxe', category: 'Weapon' },
+            { label: 'Handaxe', category: 'Weapon' },
+            { label: 'Light Hammer', category: 'Weapon' },
+            { label: 'Warhammer', category: 'Weapon' },
+          ],
+          config: {
+            header: 'Weapon Proficiencies',
+          },
         },
-      },
-      {
-        type: 'STATIC',
-        format: 'FEATURE',
-        path: 'features',
-        value: {
-          label: 'Dwarven Resilience',
-          description:
-            'You have advantage on saving throws against poison, and you have resistance against poison damage',
+        {
+          type: RACE_CONFIG_TYPE.CHOICE,
+          format: RACE_CONFIG_FORMAT.DROPDOWN,
+          path: 'MULTI',
+          options: [
+            { value: "Smith's Tools", label: "Smith's Tools" },
+            { value: "Brewer's Tools", label: "Brewer's Tools" },
+            { value: "Mason's Tools", label: "Mason's Tools" },
+          ],
+          config: {
+            header: 'Tool Proficiency',
+            getFinalValue: (val) => {
+              return {
+                otherProficiencies: [{ label: val, category: 'Tool' }],
+                customChecks: [{ label: val, stat: STATS.STR }],
+              };
+            },
+          },
         },
-        config: {
-          header: 'Speed',
+        {
+          type: RACE_CONFIG_TYPE.STATIC,
+          format: RACE_CONFIG_FORMAT.PROFICIENCY,
+          path: 'otherProficiencies',
+          value: [
+            { label: 'Common', category: 'Language' },
+            { label: 'Dwarvish', category: 'Language' },
+          ],
+          config: {
+            header: 'Languages',
+            subHeader: 'Language Proficiencies:',
+            description:
+              'You can speak, read, and write Common and Dwarvish. Dwarvish is full of hard consonants and guttural sounds, and those characteristics spill over into whatever other language a dwarf might speak.',
+          },
         },
-      },
-      {
-        type: 'STATIC',
-        format: 'FEATURE',
-        path: 'features',
-        value: {
-          label: 'Darkvision',
-          description:
-            'Accustomed to life underground, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray.',
+        {
+          type: RACE_CONFIG_TYPE.STATIC,
+          format: RACE_CONFIG_FORMAT.FEATURE,
+          path: 'features',
+          value: {
+            label: 'Dwarven Speed',
+            description: 'Your speed is not reduced by wearing heavy armor.',
+          },
         },
-      },
-      // {
-      //   type: 'CHOICE'
-      //   format: 'DROPDOWN',
-      //   path: 'subrace',
-      //   options: [
-      //     { value: 'HILL_DWARF', label: 'Hill Dwarf' },
-      //   ],
-      // }
-    ],
-    // subClassConfig: {
-
-    // }
+        {
+          type: RACE_CONFIG_TYPE.STATIC,
+          format: RACE_CONFIG_FORMAT.FEATURE,
+          path: 'features',
+          value: {
+            label: 'Stonecutting',
+            description:
+              'Whenever you make an Intelligence (History) check related to the origin of stonework, you are considered proficient in the History skill and add double your proficiency bonus to the check, instead of your normal proficiency bonus.',
+          },
+          config: {
+            header: 'Speed',
+          },
+        },
+        {
+          type: RACE_CONFIG_TYPE.STATIC,
+          format: RACE_CONFIG_FORMAT.FEATURE,
+          path: 'features',
+          value: {
+            label: 'Dwarven Resilience',
+            description:
+              'You have advantage on saving throws against poison, and you have resistance against poison damage',
+          },
+          config: {
+            header: 'Speed',
+          },
+        },
+        {
+          type: RACE_CONFIG_TYPE.STATIC,
+          format: RACE_CONFIG_FORMAT.FEATURE,
+          path: 'features',
+          value: {
+            label: 'Darkvision',
+            description:
+              'Accustomed to life underground, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray.',
+          },
+        },
+      ],
+      subClassOptions: [
+        {
+          value: 'Hill Dwarf',
+          label: 'Hill Dwarf',
+        },
+        {
+          value: 'Mountain Dwarf',
+          label: 'Hill Dwarf',
+        },
+        {
+          value: 'Hill Dwarf',
+          label: 'Hill Dwarf',
+        },
+      ],
+      subClass: [
+        {
+          type: RACE_CONFIG_TYPE.STATIC,
+          format: RACE_CONFIG_FORMAT.STATS,
+          path: 'stats',
+          value: {
+            [STATS.WIS]: 1,
+          },
+        },
+        {
+          type: RACE_CONFIG_TYPE.STATIC,
+          format: RACE_CONFIG_FORMAT.FEATURE,
+        },
+      ],
+    },
   },
   [RACES.GNOME]: {
     label: 'Gnome',
-    createConfig: [],
+    createConfig: { base: [] },
   },
   [RACES.HALF_ELF]: {
     label: 'Half Elf',
-    createConfig: [],
+    createConfig: { base: [] },
   },
   [RACES.HALFLING]: {
     label: 'Halfling',
-    createConfig: [],
+    createConfig: { base: [] },
   },
   [RACES.HALF_ORC]: {
     label: 'Half Orc',
-    createConfig: [],
+    createConfig: { base: [] },
   },
   [RACES.HUMAN]: {
     label: 'Human',
-    createConfig: [],
+    createConfig: { base: [] },
   },
   [RACES.TIEFLING]: {
     label: 'Tiefling',
-    createConfig: [],
+    createConfig: { base: [] },
   },
   [RACES.TASHA_CUSTOM]: {
     label: "Tasha's Custom Lineage",
-    createConfig: [],
+    createConfig: { base: [] },
   },
   [RACES.CUSTOM]: {
     label: 'Custom',
-    createConfig: [],
+    createConfig: { base: [] },
   },
 };
 
